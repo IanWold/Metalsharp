489e13e10cac265702298f34eee1e7befe95d541
Ian Wold <ilwold@hotmail.com>
2018-11-15 14:03:01 -0600

    Add the Debug plugin

---
+++ Metalsharp/Plugins/Debug.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Metal.Sharp
+{
+    /// <summary>
+    /// The Debug plugin
+    /// 
+    /// Writes a log after every Use, outputting the contents of the input and output directories.
+    /// </summary>
+    public class Debug : IMetalsharpPlugin
+    {
+        /// <summary>
+        /// By default, write debug logs with Debug.WriteLine()
+        /// </summary>
+        public Debug() : this(message => System.Diagnostics.Debug.WriteLine(message)) { }
+
+        /// <summary>
+        /// Configure Debug to write logs to a log file
+        /// </summary>
+        /// <param name="logPath">The path to the log file</param>
+        public Debug(string logPath)
+            : this(message =>
+        {
+            using (var writer = new StreamWriter(logPath, true))
+            {
+                writer.WriteLineAsync(message);
+            }
+        })
+        { }
+
+        /// <summary>
+        /// Configure Debug to use custom behavior when writing logs
+        /// </summary>
+        /// <param name="onLog">The action to execute when writing a log</param>
+        public Debug(Action<string> onLog) =>
+            OnLog = onLog;
+
+        /// <summary>
+        /// The action to execute when writing a log
+        /// </summary>
+        private Action<string> OnLog;
+
+        /// <summary>
+        /// A count of the number of "Uses" against the directory
+        /// </summary>
+        private int Uses = 0;
+
+        /// <summary>
+        /// Invokes the plugin
+        /// </summary>
+        /// <param name="directory"></param>
+        public void Execute(Metalsharp directory) =>
+            directory.AfterUse += (sender, e) =>
+                OnLog("Step " + ++Uses + "." +
+                    "\r\n" +
+                    "Input directory:" +
+                    "\r\n\r\n" +
+                    WriteDirectory(directory.InputFiles) +
+                    "\r\n\r\n" +
+                    "Output directory:" +
+                    "\r\n\r\n" +
+                    WriteDirectory(directory.OutputFiles) +
+                    "\r\n\r\n" +
+                    "---" +
+                    "\r\n\r\n"
+                );
+
+        /// <summary>
+        /// Prettify the contents of a collection of files
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns></returns>
+        private string WriteDirectory(IMetalsharpFileCollection<MetalsharpFile> directory) =>
+            string.Join("\r\n",
+                directory
+                    .OrderBy(file => file.FilePath)
+                    .Select(file => "\t" + file.FilePath)
+            );
+    }
+}
--- Metalsharp/Plugins/MetalsharpExtensions.cs
+++ Metalsharp/Plugins/MetalsharpExtensions.cs
@@ -16,6 +16,32 @@ namespace Metal.Sharp
             directory.Use(new Branch(branches));
 
         /// <summary>
+        /// Invoke the default Debug plugin
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns></returns>
+        public static Metalsharp UseDebug(this Metalsharp directory) =>
+            directory.Use(new Debug());
+
+        /// <summary>
+        /// Invoke the Debug plugin with a log file to capture the debug logs
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="logPath">The path to the log file</param>
+        /// <returns></returns>
+        public static Metalsharp UseDebug(this Metalsharp directory, string logPath) =>
+            directory.Use(new Debug(logPath));
+
+        /// <summary>
+        /// Invoke the Debug plugin with custom log behavior
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="onLog">The action to execute to log a debug line</param>
+        /// <returns></returns>
+        public static Metalsharp UseDebug(this Metalsharp directory, Action<string> onLog) =>
+            directory.Use(new Debug(onLog));
+
+        /// <summary>
         /// Invoke the Drafts plugin
         /// </summary>
         /// <returns></returns>
