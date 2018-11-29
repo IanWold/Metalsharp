6f9299c34f993f990d9c599bd6be40c9027fe575
Ian Wold <ilwold@hotmail.com>
2018-11-29 16:51:31 -0600

    Fix markdown plugin file selection bug

--- Metalsharp/Plugins/Markdown.cs
+++ Metalsharp/Plugins/Markdown.cs
@@ -17,11 +17,9 @@ namespace Metalsharp
         {
             foreach (var file in directory.InputFiles)
             {
-                if (file.Extension == "md" || file.Extension == "markdown")
+                if (file.Extension == ".md" || file.Extension == ".markdown")
                 {
-                    var name = Path.GetFileNameWithoutExtension(file.FilePath);
-                    var text = Markdig.Markdown.ToHtml(file.Text);
-                    directory.OutputFiles.Add(new MetalsharpFile(text, name + ".html") { Metadata = file.Metadata });
+                    directory.OutputFiles.Add(new MetalsharpFile(Markdig.Markdown.ToHtml(file.Text), Path.Combine(file.Directory, file.Name + ".html")));
                 }
             }
         }
