4c8e0d35183321cb3dcb070516845f8e7c7173c8
Ian Wold <ilwold@hotmail.com>
2018-12-06 17:11:55 -0600

    Add comments to IMetalsharpPlugin

--- Metalsharp/Interfaces/IMetalsharpPlugin.cs
+++ Metalsharp/Interfaces/IMetalsharpPlugin.cs
@@ -1,15 +1,38 @@
 ﻿namespace Metalsharp
 {
     /// <summary>
-    /// Represents a Metalsharp plugin
+    ///     The interface from which Metalsharp plugin must (read: should) derive. 
     /// </summary>
+    /// <example>
+    ///     Implementing a Metalsharp plugin is as easy as implementing this interface:
+    ///     
+    ///     ```c#
+    ///         public class DeleteEverything : IMetalsharpPlugin
+    ///         {
+    ///         
+    ///         public void Execute(MetalsharpDirectory directory) =>
+    ///         directory.RemoveFiles(file => true);
+    /// 
+    ///         }
+    ///     ```
+    ///     
+    ///     This plugin can then be used like any other:
+    ///     
+    ///     ```c#
+    ///         new MetalsharpDirectory()
+    ///         ... // Add files
+    ///         .Use&lt;DeleteEverything&gt;();
+    ///     ```
+    /// </example>
     public interface IMetalsharpPlugin
     {
         /// <summary>
-        /// Invokes the plugin. Called by Metalsharp.Use
+        ///     Invokes the plugin. `Called by Metalsharp.Use`.
         /// </summary>
-        /// <param name="directory">The directory to alter</param>
-        /// <returns>The same directory as was input</returns>
+        /// 
+        /// <param name="directory">
+        ///     The directory to alter.
+        /// </param>
         void Execute(MetalsharpDirectory directory);
     }
 }
