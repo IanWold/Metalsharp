002e74d802858a218b58c3352c046780b7e5ad74
Ian Wold <ilwold@hotmail.com>
2022-08-14 13:29:50 -0500

    Break out plugin tests to separate classes

---
+++ Metalsharp.Tests/Plugins/LevellerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Metalsharp.Tests;
+
+public class LevellerTests
+{
+	[Fact]
+	public void LevellerAddsLevelMetadata()
+	{
+		var file = new MetalsharpFile("", "file");
+		_ = new MetalsharpProject().AddInput(file).UseLeveller();
+
+		Assert.Contains(file.Metadata, m => m.Key == "level");
+	}
+
+	[Fact]
+	public void LevellerCorrectlyLevelsFiles()
+	{
+		var project = new MetalsharpProject().AddInput("Scenario\\Directory2", "").UseLeveller();
+
+		Assert.Equal(1, project.InputFiles.Single(f => f.Name == "file10").Metadata["level"]);
+		Assert.Equal(2, project.InputFiles.Single(f => f.Name == "file11").Metadata["level"]);
+	}
+}
