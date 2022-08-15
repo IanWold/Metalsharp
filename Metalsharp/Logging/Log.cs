b51914c946592efee3337f638c6338be83a81106
Ian Wold <1645320+IanWold@users.noreply.github.com>
2022-08-14 23:16:03 -0500

    Prettify log output (#12)
    * Prettify log output

--- Metalsharp/Logging/Log.cs
+++ Metalsharp/Logging/Log.cs
@@ -8,18 +8,14 @@ namespace Metalsharp.Logging;
 /// </summary>
 public class Log
 {
-	private readonly LogLevel _level;
+	private readonly LogLevel _verbosity;
 
-	internal Log(LogLevel level, MetalsharpProject project)
+	private bool _wasLastLogSection = false;
+
+	internal Log(LogLevel level)
 	{
-		_level = level;
-		Project = project;
+		_verbosity = level;
 	}
-
-	/// <summary>
-	///		The `MetalsharpProject` to which this log belongs.
-	/// </summary>
-	public MetalsharpProject Project { get; init; }
 
 	/// <summary>
 	///		Invoked when any message is sent to the logger, irrespective of the specified log level.
@@ -35,10 +31,12 @@ public class Log
 	{
 		OnAnyLog?.Invoke(this, logEventArgs);
 
-		if (logEventArgs.Level >= _level)
+		if (logEventArgs.Level >= _verbosity)
 		{
 			Console.WriteLine(logEventArgs.Message);
 			OnLog?.Invoke(this, logEventArgs);
+
+			_wasLastLogSection = false;
 		}
 	}
 
@@ -50,7 +48,7 @@ public class Log
 	///		The message to log.
 	///	</param>
 	public void Debug(string message) =>
-		TryLog(new(LogLevel.Debug, message));
+		TryLog(new(LogLevel.Debug, message.Trim()));
 
 	/// <summary>
 	///		Log a message at `Info` level.
@@ -60,7 +58,33 @@ public class Log
 	///		The message to log.
 	/// </param>
 	public void Info(string message) =>
+		TryLog(new(LogLevel.Info, message.Trim()));
+
+	/// <summary>
+	///		Log a message at `Info` level as a section, separated cleanly by a single empty line.
+	/// </summary>
+	/// 
+	/// <param name="message">
+	///		The message to log.
+	///	</param>
+	internal void InfoSection(string message)
+	{
+		TryLog(new(LogLevel.Info, $"{(_wasLastLogSection ? "" : "\n")}{message.Trim()}\n"));
+		_wasLastLogSection = true;
+	}
+
+	/// <summary>
+	///		Log a message at `Info` level directly, without trimming.
+	/// </summary>
+	/// 
+	/// <param name="message">
+	///		The message to log.
+	///	</param>
+	internal void InfoDirect(string message)
+	{
 		TryLog(new(LogLevel.Info, message));
+		_wasLastLogSection = true;
+	}
 
 	/// <summary>
 	///		Log a message at `Error` level.
@@ -70,7 +94,7 @@ public class Log
 	///		The message to log.
 	/// </param>
 	public void Error(string message) =>
-		TryLog(new(LogLevel.Error, message));
+		TryLog(new(LogLevel.Error, message.Trim()));
 
 	/// <summary>
 	///		Log a message at `Fatal` level.
@@ -80,5 +104,5 @@ public class Log
 	///		The message to log.
 	/// </param>
 	public void Fatal(string message) =>
-		TryLog(new(LogLevel.Fatal, message));
+		TryLog(new(LogLevel.Fatal, message.Trim()));
 }
